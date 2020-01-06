a2f8ca47e43c8ba9fd36b49bbbcd70efd0bac761
Kazuaki Tanida <punigumi@gmail.com>
2020-01-07 00:52:36 +0900

    Add -n option

--- README.md
+++ README.md
@@ -9,11 +9,13 @@ Usage
 ---
 
 ```sh
-poll [-s SECONDS] IN OUT
+poll [-n COUNTS] [-s SECONDS] IN OUT
 ```
 
 Example
 ---
+
+Check if a file at `/path/to/watch` is changed every 60 seconds.
 
 ```sh
 poll -s 60 'cat /path/to/watch' 'beep -f 100 -l 500'
@@ -31,4 +33,4 @@ fisher add slaypni/poll
 System Requirements
 ---
 
-- [fish](https://github.com/fishshell) 2.7.0+
+- [fish](https://github.com/fishshell) 2.7+
--- poll.fish
+++ poll.fish
@@ -1,41 +1,55 @@
 function poll
     function __print_help
         printf "Usage:\n"
-        printf "    poll [-s SECONDS] IN OUT\n\n"
+        printf "    poll [-n COUNTS] [-s SECONDS] IN OUT\n\n"
         printf "Example:\n"
         printf "    poll -s 60 'cat /path/to/watch' 'beep -f 100 -l 500'\n\n"
         printf "Args:\n"
         printf "    IN            Command to be run for polling\n"
         printf "    OUT           Command to be run when output of the last polling was changed\n\n"
         printf "Options:\n"
+        printf "    -n --num      Max number of attempts for polling\n"
         printf "    -s --sleep    Interval between polling\n"
         printf "    -h --help     Print this help\n"
     end
 
-    set -l sleep_seconds 0
-    argparse 's/sleep=' 'h/help' -- $argv
-    if set -q _flag_help
-        __print_help
-        return 0
-    end
-    if set -q _flag_sleep
-        set sleep_seconds $_flag_sleep
-    end
+    argparse 's/sleep=' 'n/num=' 'h/help' -- $argv    
+    
     if not set -q argv[2]
         __print_help
         return 1
     end
+
+    if set -q _flag_help
+        __print_help
+        return 0
+    end
+
+    set -l sleep_seconds 0
+    if set -q _flag_sleep
+        set sleep_seconds $_flag_sleep
+    end
+
+    set -l max_counter 0
+    if set -q _flag_num
+        set max_counter $_flag_num
+    end
+
     set -l poll_cmd $argv[1]
     set -l triggered_cmd $argv[2]
-
+    set -l counter 0
     while true
         set last_stdout (eval $poll_cmd)
         if set -q prev_stdout
             if [ (echo $prev_stdout) != (echo $last_stdout) ]
                 eval $triggered_cmd
             end
+            set counter (math $counter + 1)
         end
         set prev_stdout $last_stdout
+        if [ $max_counter -gt 0 ] && [ $counter -ge $max_counter ]
+            break
+        end
         sleep $sleep_seconds
     end
 end
